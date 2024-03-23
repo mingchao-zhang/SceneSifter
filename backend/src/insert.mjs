fa4cd589dbeb6b8dce55a3b23d656211f9eb24c1
Mingchao Zhang <zmcmarkzhang@gmail.com>
2024-03-22 17:26:06 -0700

    search working

--- backend/src/insert.mjs
+++ backend/src/insert.mjs
@@ -25,7 +25,7 @@ let test_intervals = [
     {"video_name": "Video 1",
     "start_time": '2024-03-18 10:00:00', 
      "end_time": '2024-03-18 10:30:00',
-     "description": 'This is the first video.'
+     "description": 'random text number 1'
     },
     {
     "video_name": "Video 2",
@@ -37,22 +37,13 @@ let test_intervals = [
     "video_name": "Video 3",
     "start_time": '2024-03-18 12:00:00', 
     "end_time": '2024-03-18 12:30:00',
-    "description": 'This is the second video.'
+    "description": 'This is the third video.'
    }
 ]
 
-async function append_vectors_to_intervals(intervals) {
-    // create a feature extraction pipeline
-    const extractor = await pipeline('feature-extraction', 'nomic-ai/nomic-embed-text-v1.5', {
-        quantized: false, // Comment out this line to use the quantized version
-    });
-    const descriptions = []
-    for (let i = 0; i < intervals.length; ++i) {
-        let interval = intervals[i];
-        descriptions.push(interval.description)
-    }
+async function texts_to_vectors(extractor, texts) {
     // compute sentence embeddings
-    let embeddings = await extractor(descriptions, { pooling: 'mean' });
+    let embeddings = await extractor(texts, { pooling: 'mean' });
     // normalize
     const matryoshka_dim = 512;
     embeddings = layer_norm(embeddings, [embeddings.dims[1]])
@@ -60,9 +51,25 @@ async function append_vectors_to_intervals(intervals) {
     .normalize(2, -1)
     .tolist()
 
-    // insert generated vectors to intervals
+    return embeddings
+}
+
+async function text_to_vector(extractor, text) {
+    let embeddings = await texts_to_vectors(extractor, [text])
+    return embeddings[0];
+}
+
+async function append_vectors_to_intervals(extractor, intervals) {
+    // 1. get all descriptions from video listing items
+    const descriptions = []
     for (let i = 0; i < intervals.length; ++i) {
-        
+        let interval = intervals[i];
+        descriptions.push(interval.description)
+    }
+    // 2. text to vectors
+    let embeddings = await texts_to_vectors(extractor, descriptions)
+    // 3. insert generated vectors to intervals
+    for (let i = 0; i < intervals.length; ++i) {
         intervals[i]['description_embedding'] = embeddings[i]
     }
 }
@@ -82,18 +89,48 @@ async function insert(client, intervals) {
         value_string += sub_string
     }
     value_string = value_string.slice(0, -2);
-    let query_string = "INSERT INTO video_listing (video_name, start_time, end_time, description, description_embedding) VALUES \n" + value_string
+    let queryStr = "INSERT INTO video_listing (video_name, start_time, end_time, description, description_embedding) VALUES \n" + value_string
     // execute the query string
-    const res = await client.query(query_string);
-    console.log(res)
+    const res = await client.query(queryStr);
+}
+
+async function search(client, inputVec) {
+    let queryStr = `SELECT video_name, description, 
+                        1 - (description_embedding <=> '[${inputVec}]') AS similarity
+                    FROM video_listing 
+                    ORDER BY description_embedding <=> '[${inputVec}]'
+                    LIMIT 2`
+    const res = await client.query(queryStr)
+
+
+    let videoIntervals = [];
+    for (let i = 0; i < res.rows.length; i++) {
+        const row = res.rows[i];
+
+        videoIntervals.push(
+            { "video_name": row.video_name, 
+            "description": row.description,
+            "similarity": row.similarity });
+
+        console.log("Returned: ", `${row.video_name}, ${row.description}, ${row.similarity}\n`);
+    }
 }
 
 async function main() {
     // create a database
     const client = new Client(pgEndpoint);
     await client.connect();
-    await append_vectors_to_intervals(test_intervals)
+    // create a converter that converts texts to vectors
+    const extractor = await pipeline('feature-extraction', 'nomic-ai/nomic-embed-text-v1.5', {
+        quantized: false, // Comment out this line to use the quantized version
+    });
 
-    insert(client, test_intervals)
+    // insert test intervals
+    await append_vectors_to_intervals(extractor, test_intervals)
+    // insert(client, test_intervals)
+
+    // try searching a test video interval
+    let test_search_vector = await text_to_vector(extractor, 'random')
+    search(client, test_search_vector)
 }
 main()
